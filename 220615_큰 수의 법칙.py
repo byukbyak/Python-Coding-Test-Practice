a569db352797875eb8a27a779d2bfbb4fcdad8f3
byukbyak <95122510+byukbyak@users.noreply.github.com>
2022-06-15 16:24:34 +0900

    Update 220615_큰 수의 법칙.py

--- 220615_큰 수의 법칙.py
+++ 220615_큰 수의 법칙.py
@@ -19,3 +19,26 @@ while True:
   m -= 1 # 더할 때마다 1씩 빼기
 
 print(result) # 최종 답안 출력
+
+#######################################################
+
+# 배열의 크기 n 숫자가 더해지는 횟수 m, 같은 수를 한번에 더할 수 있는 최대값 k를 공백으로 구분하여 입력받기
+n, m, k = map(int, input().split())
+# N개의 수를 공백으로 구분하여 입력받기
+data = list(map(int, input().split()))
+
+data.sort() # 입력받은 수 정렬
+first = data[n - 1] # 가장 큰 수
+second = data[n - 2] # 두번째로 큰 수
+
+result = 0
+
+# 가장 큰 수가 더해지는 횟수 계산
+count = int(m / (k + 1)) * k
+count += m % (k+ 1)
+
+result = 0
+result += (count) * first # 가장 큰 수 더하기
+result += (m - count) * second #두번째로 큰 수 더하기
+
+print(result) # 최종 답안 출력
