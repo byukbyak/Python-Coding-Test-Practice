666e09e9524eafc175b1bf969e1186cff813a09b
Debby <thesp72@naver.com>
2022-06-23 02:13:26 +0900

    220622_이코테 정렬

---
+++ 00_삽입정렬.py
@@ -0,0 +1,15 @@
+# 삽입 정렬
+# 평균 시간 복잡도 : O(N^2)
+# 공간 복잡도 : O(N)
+# 특징 : 데이터가 거의 정렬되어 있을 때는 가장 빠르다
+
+array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+for i in range(1, len(array)):
+    for j in range(i, 0, -1): # 인덱스 i부터 1까지 1씩 감소하며 반복하는 문법
+        if array[j] < array[j - 1]: # 한 칸씩 왼쪽으로 이동
+            array[j], array[j - 1] = array[j - 1], array[j]
+        else: # 자기보다 작은 데이터를 만나면 그 위치에서 멈춤
+            break
+
+print(array)
