8d552ad946fcb0bd78ab383844e551d757f6a9be
Debby <thesp72@naver.com>
2022-06-25 19:54:03 +0900

    220625

--- 2206/25/키패드 누르기.py
+++ 2206/25/키패드 누르기.py
@@ -2,7 +2,6 @@ def get_distance(keypad, finget_position, next_number):
     
     next_number_position = keypad[next_number]
     distance = abs(finget_position[0] - next_number_position[0]) + abs(finget_position[1] - next_number_position[1])
-    print(f"next_number_position : {next_number_position}, distance : {distance}")
     
     return distance
     
@@ -11,7 +10,7 @@ def solution(numbers, hand):
     
     # 현재 위치를 파악하기 위한 딕셔너리
     keypad = {
-         1 :(0,0), 2 :(0,1), 3 :(0,3),
+         1 :(0,0), 2 :(0,1), 3 :(0,2),
          4 :(1,0), 5 :(1,1), 6 :(1,2),
          7 :(2,0), 8 :(2,1), 9 :(2,2),
         '*':(3,0), 0 :(3,1),'#':(3,2)
@@ -23,48 +22,54 @@ def solution(numbers, hand):
     
     left_finger_position = keypad['*']
     right_finger_position = keypad['#']
-    print(f"left_finger_position : {left_finger_position}, right_finger_position : {right_finger_position}")
+    print(f"0. 왼손 위치 : {left_finger_position}, 오른손 위치 : {right_finger_position}\n")
     
     for number in numbers:
         if number in left_finger_numbers:
             result += 'L'
             left_finger_position = keypad[number]
-            print(f"1. result : {result}")
-            print(f"1. left_finger_position : {left_finger_position}, right_finger_position : {right_finger_position}")
+            print(f"1. 왼손 위치 : {left_finger_position}, 오른손 위치 : {right_finger_position}")
+            print(f"[ 번호 : {number} ] [ 결과 : {result} ]\n")
         elif number in right_finger_numbers:
             result += 'R'
             right_finger_position = keypad[number]
-            print(f"2. result : {result}")
-            print(f"2. left_finger_position : {left_finger_position}, right_finger_position : {right_finger_position}")
+            print(f"2. 왼손 위치 : {left_finger_position}, 오른손 위치 : {right_finger_position}")
+            print(f"[ 번호 : {number} ] [ 결과 : {result} ]\n")
         else:
             left_finger_distance = get_distance(keypad, left_finger_position, number)
             right_finger_distance = get_distance(keypad, right_finger_position, number)
+            print(f">> [다음 번호 : {number}]\n>> [다음 포지션 : {keypad[number]}]\n>> [왼손 거리 : {left_finger_distance}, 오른손 거리 : {right_finger_distance}]\n")
 
             if left_finger_distance > right_finger_distance:
                 result += 'R'
                 right_finger_position = keypad[number]
-                print(f"3. result : {result}")
-                print(f"3. left_finger_position : {left_finger_position}, right_finger_position : {right_finger_position}")
+                print(f"3. 왼손 위치 : {left_finger_position}, 오른손 위치 : {right_finger_position}")
+                print(f"[ 번호 : {number} ] [ 결과 : {result} ]\n")
+                
             elif left_finger_distance < right_finger_distance:
                 result += 'L'
-                right_finger_position = keypad[number]
-                print(f"4. result : {result}")
-                print(f"4. left_finger_position : {left_finger_position}, right_finger_position : {right_finger_position}")
+                left_finger_position = keypad[number]
+                print(f"4. 왼손 위치 : {left_finger_position}, 오른손 위치 : {right_finger_position}")
+                print(f"[ 번호 : {number} ] [ 결과 : {result} ]\n")
+                
             elif left_finger_distance == right_finger_distance:
                 result += hand[0].upper()
+                print(f"(( 주 손잡이 : {hand[0].upper()} ))\n")
+                
                 if hand == 'right':
                     right_finger_position = keypad[number]
-                    print(f"5. result : {result}")
-                    print(f"5. left_finger_position : {left_finger_position}, right_finger_position : {right_finger_position}")
+                    print(f"5. 왼손 위치 : {left_finger_position}, 오른손 위치 : {right_finger_position}")
+                    print(f"[ 번호 : {number} ] [ 결과 : {result} ]\n")
+                    
                 elif hand == 'left':
                     left_finger_position = keypad[number]
-                    print(f"6. result : {result}")
-                    print(f"6. left_finger_position : {left_finger_position}, right_finger_position : {right_finger_position}")
+                    print(f"6. 왼손 위치 : {left_finger_position}, 오른손 위치 : {right_finger_position}")
+                    print(f"[ 번호 : {number} ] [ 결과 : {result} ]\n")
                     
     return result
 
-numbers = [1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5]
-hand = "right"
+numbers = [7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2]
+hand = "left"
 print(solution(numbers, hand))
 
     # 테스트 1
